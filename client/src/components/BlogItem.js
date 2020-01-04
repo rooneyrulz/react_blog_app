7aa4274f5d90004fb22bf40a19117b6d6745e66a
ruzny <ruzny@ruzny.com>
2020-01-04 20:52:15 +0530

    modifying blogitem layout

--- client/src/components/BlogItem.js
+++ client/src/components/BlogItem.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BlogItem = ({ blog }) => {
+  const onDelete = id => console.log(id);
+
   return (
     <div className='card mb-2'>
-      <div className='card-body'>
-        <p>{blog.title}</p>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+        className='card-body blog-item-body'
+      >
+        <Link
+          to={`/blog/${blog._id}`}
+          style={{ textDecoration: 'none', color: '#111' }}
+        >
+          <p className='lead mb-0'>{blog.title}</p>
+        </Link>
+        <div className='card-action'>
+          <Link to='/blog/edit' className='btn btn-info btn-sm'>
+            Edit
+          </Link>
+          <button
+            onClick={e => onDelete(blog._id)}
+            className='btn btn-danger btn-sm ml-1'
+          >
+            Remove
+          </button>
+        </div>
       </div>
     </div>
   );
